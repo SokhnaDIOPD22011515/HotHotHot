f76cb36a1b106d8b00d3e98b6b375d2d88da20e2
ROUBAUD-Axel-2225073b <axel.roubaud@etu.univ-amu.fr>
2024-02-18 22:36:44 +0100

    Commit pour ratrapper l'erreur de Sosso

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -56,12 +56,12 @@ document.addEventListener('DOMContentLoaded', async () => {
         });
     });
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service Worker enregistré avec succès : ', registration);
-            })
-            .catch(error => {
-                console.error("Erreur lors de l'enregistrement du Service Worker : ", error);
-            });
-    }
+    navigator.serviceWorker.register('./service-worker.js')
+        .then(registration => {
+            console.log('Service Worker enregistré avec succès : ', registration);
+        })
+        .catch(error => {
+            console.error("Erreur lors de l'enregistrement du Service Worker : ", error);
+        });
+}
 });
